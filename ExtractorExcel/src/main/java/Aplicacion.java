2fded647012462ab35cfbbe34a795dc9a6e0b412
javabuk <joprieto@gmail.com>
2018-04-15 18:57:12 +0200

    Eliminando imports sin utilizar.

--- ExtractorExcel/src/main/java/Aplicacion.java
+++ ExtractorExcel/src/main/java/Aplicacion.java
@@ -5,9 +5,7 @@ import org.correlaciones.repository.RepositorioFicheroExcelImpl;
 import org.correlaciones.services.SalidaFicheroSql;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
-//@ComponentScan({"org.correlaciones"})
 public class Aplicacion {
 
 	public static void main(String[] args) {
--- ExtractorExcel/src/main/java/org/correlaciones/services/SalidaFicheroSql.java
+++ ExtractorExcel/src/main/java/org/correlaciones/services/SalidaFicheroSql.java
@@ -1,9 +1,6 @@
 package org.correlaciones.services;
 
 import java.io.FileNotFoundException;
-import java.util.List;
-
-import org.correlaciones.model.Correlacion;
 
 public interface SalidaFicheroSql {
 
